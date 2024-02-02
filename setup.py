f041ffadd27e5b659fe61f800c9d01b864eb8e76
RafaelTakata0105 <rafaeltakata0105@gmail.com>
2024-02-02 08:10:26 -0600

    cookiecutter instalacion

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Lectures of Data Analytics Course',
+    author='RafaelTakata0105',
+    license='MIT',
+)
